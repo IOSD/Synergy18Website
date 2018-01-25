49e42708552e236d40f22e2b02b0a24f34fb3c9f
Dhruv Ramdev <pythonbrilliant@gmail.com>
2018-01-26 02:58:11 +0530

    Added Form

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,17 +1,18 @@
+$(document).ready(function () {
+
 // portfolio
-$('.gallery ul li a').click(function() {
-    var itemID = $(this).attr('href');
-    $('.gallery ul').addClass('item_open');
-    $(itemID).addClass('item_open');
-    return false;
-});
-$('.close').click(function() {
-    $('.port, .gallery ul').removeClass('item_open');
-    return false;
+    $('.gallery ul li a').click(function() {
+        var itemID = $(this).attr('href');
+        $('.gallery ul').addClass('item_open');
+        $('.lectures-wrapper').addClass('noscroll');
+        $(itemID).addClass('item_open');
+        return false;
+    });
+    $('.close').click(function() {
+        $('.port, .gallery ul').removeClass('item_open');
+        $('.lectures-wrapper').removeClass('noscroll');
+        return false;
+    });
+
 });
 
-$(".gallery ul li a").click(function() {
-    $('html, body').animate({
-        scrollTop: parseInt($("#top").offset().top)
-    }, 400);
-});
